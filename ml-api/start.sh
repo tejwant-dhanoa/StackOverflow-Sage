b9cd9fd4344826e88211aa8ce1b594e46e88a288
tejwantdhanoa <dhanoatwk@gmail.com>
2025-07-12 01:01:41 +0530

    Fix: remove pyenv, use Render-compatible start script

--- ml-api/start.sh
+++ ml-api/start.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Load pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-# Set correct Python version
-pyenv shell 3.11.9
-
-# Run your app with gunicorn
+echo "Starting Gunicorn server..."
 gunicorn app:app --bind 0.0.0.0:$PORT
