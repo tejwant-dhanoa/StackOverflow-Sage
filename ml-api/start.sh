e1956f826364c637aa8f16097ab6dbc6de48ac43
tejwantdhanoa <dhanoatwk@gmail.com>
2025-07-12 00:49:30 +0530

    Fix: activate pyenv in start.sh

--- ml-api/start.sh
+++ ml-api/start.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+# Load pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# Set correct Python version
+pyenv shell 3.11.9
+
+# Run your app with gunicorn
 gunicorn app:app --bind 0.0.0.0:$PORT
