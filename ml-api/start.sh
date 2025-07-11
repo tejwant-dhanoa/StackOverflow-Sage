a2aa4d77608b0a764652381a2924b5ab54775a81
tejwantdhanoa <dhanoatwk@gmail.com>
2025-07-12 01:19:32 +0530

    fix use python -m

--- ml-api/start.sh
+++ ml-api/start.sh
@@ -3,4 +3,4 @@
 echo "Starting Gunicorn server..."
 
 # Use the full path to the gunicorn installed in the venv
-python3 -m gunicorn app:app --bind 0.0.0.0:$PORT
+python -m gunicorn app:app --bind 0.0.0.0:$PORT
