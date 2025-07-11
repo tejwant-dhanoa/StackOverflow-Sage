b5a1a6e8d8c5b514b51b9758639cd115e13e3c4e
tejwantdhanoa <dhanoatwk@gmail.com>
2025-07-12 00:42:25 +0530

    Make build.sh executable

--- ml-api/build.sh
+++ ml-api/build.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
-# Tell Render to use Python version from runtime.txt
-echo "Using Python $(cat runtime.txt)"
+
+# Fail on first error
+set -o errexit
+
+# Install Python 3.11 using pyenv
+PYTHON_VERSION=3.11.9
+
+echo "Installing Python $PYTHON_VERSION with pyenv..."
+curl https://pyenv.run | bash
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+pyenv install -s $PYTHON_VERSION
+pyenv global $PYTHON_VERSION
+
+# Install pip and project requirements
+pip install --upgrade pip
+pip install -r requirements.txt
