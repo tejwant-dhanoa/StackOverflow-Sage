109a26f58b312bd3186313297ed445eff3fb61fa
tejwantdhanoa <dhanoatwk@gmail.com>
2025-07-12 16:13:09 +0530

    making fixes

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,6 +2,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL + "/api",
   withCredentials: true, // only if using cookies
 });
